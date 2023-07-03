4c98b301a9f51c34e0f3ea60ac1959fd52264975
CMisch <71983546+CMisch@users.noreply.github.com>
2023-07-03 09:05:04 -0700

    Update U08_Extract HTML links.py
    added extra comment and erased previous target website

--- U08_Extract HTML links.py
+++ U08_Extract HTML links.py
@@ -1,16 +1,16 @@
 #U8A1 part 3
 #Extract HTML links
 #This program will use Beautiful Soup to write a 
-# Python program that will extract all of the HTML links from http://www.capella.edu.
+# Python program that will extract all of the HTML links from a target
 
 from bs4 import BeautifulSoup as bs
 import requests
 
 #Get website using an HTTP client 
-url = 'https://www.capella.edu'
+url = 'https' #add your own target web address
 html = requests.get(url).text
 
-#Use beautiful soup to parse html from capella.edu and create an object
+#Use beautiful soup to parse html and create an object
 soup = bs(html, 'html.parser' )
 
 #find and print all href tags
